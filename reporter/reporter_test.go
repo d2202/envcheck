7540685d8672ce005c4355569ef86932bf3dcff0
Denis Besfamilnyy <d.besfamilnyy@dengi-vpered.ru>
2026-07-07 11:06:37 +0300

    add report JSON test

--- reporter/reporter_test.go
+++ reporter/reporter_test.go
@@ -2,6 +2,8 @@ package reporter
 
 import (
 	"bytes"
+	"encoding/json"
+	"slices"
 	"strings"
 	"testing"
 
@@ -62,5 +64,74 @@ func TestReporter(t *testing.T) {
 				t.Errorf("test reporter: got %q, want: %q", got, tt.want)
 			}
 		})
+	}
+}
+
+func TestReportJSON(t *testing.T) {
+	var tests = []struct {
+		name string
+		in   checker.CheckResult
+		want JSONReport
+	}{
+		{"no issues",
+			checker.CheckResult{},
+			JSONReport{OK: true},
+		},
+		{"missing",
+			checker.CheckResult{
+				Issues: []checker.Issue{{Key: "asd", Kind: checker.Missing}},
+			},
+			JSONReport{Missing: []string{"asd"}},
+		},
+		{"extra",
+			checker.CheckResult{
+				Issues: []checker.Issue{{Key: "asd", Kind: checker.Extra}},
+			},
+			JSONReport{Extra: []string{"asd"}},
+		},
+		{"empty",
+			checker.CheckResult{
+				Issues: []checker.Issue{{Key: "asd", Kind: checker.Empty}},
+			},
+			JSONReport{Empty: []string{"asd"}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var (
+				buf bytes.Buffer
+				got JSONReport
+			)
+
+			ReportJSON(ReportInput{
+				Result: tt.in,
+				//len и пути не важны
+				ExamplePath:    "path/to/.example.env",
+				EnvPath:        "path/to/.env",
+				ExampleKeysLen: 2,
+				EnvKeysLen:     2,
+			}, &buf)
+
+			if err := json.NewDecoder(&buf).Decode(&got); err != nil {
+				t.Fatalf("failed to decode JSON: %v", err)
+			}
+
+			if got.OK != tt.want.OK {
+				t.Errorf("test report json: got: %t, want: %t", got.OK, tt.want.OK)
+			}
+
+			compareResultFields(t, got.Missing, tt.want.Missing, "missing")
+			compareResultFields(t, got.Empty, tt.want.Empty, "empty")
+			compareResultFields(t, got.Extra, tt.want.Extra, "extra")
+		})
+	}
+}
+
+func compareResultFields(t *testing.T, got, want []string, field string) {
+	t.Helper()
+	slices.Sort(got)
+	slices.Sort(want)
+	if !slices.Equal(got, want) {
+		t.Errorf("%s: got %v, want %v", field, got, want)
 	}
 }
